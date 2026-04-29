62254ff2c10ff561403e4d6c2fe365f229230a73
Vitor de Barros Rigueira Fernandes <vbrfernandes@sga.pucminas.br>
2026-04-29 16:32:27 -0300

    commit novo projeto em c#

---
+++ src/SOS_dog/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using SOS_dog.Models;
+using System.Diagnostics;
+
+namespace SOS_dog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/SOS_dog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SOS_dog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
