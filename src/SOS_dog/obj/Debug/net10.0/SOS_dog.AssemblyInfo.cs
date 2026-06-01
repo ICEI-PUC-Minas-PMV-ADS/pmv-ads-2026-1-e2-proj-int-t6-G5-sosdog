8c32c06fad4e357748b2d0482645fd5c769efd35
Vitor de Barros Rigueira Fernandes <vbrfernandes@sga.pucminas.br>
2026-06-01 16:33:27 -0300

    commit 01/06/26

--- src/SOS_dog/obj/Debug/net10.0/SOS_dog.AssemblyInfo.cs
+++ src/SOS_dog/obj/Debug/net10.0/SOS_dog.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SOS_dog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a884921d3002238018472251efe9bd16bc7d959")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c148b0b0c6e50234e9064692e8bb6b71e6de235a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SOS_dog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SOS_dog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
